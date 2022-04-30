4c7860591721a2b3a9b06763c74e59526f226785
shs <shs3832@gmail.com>
2022-04-30 22:33:30 +0900

    # dev server 추가 - 로컬에서 확인가능하도록 dev 서버 패키지 추가 - dist 폴더를 바라보게 세팅 html 파일경로로 들어가줘야함
    # watch 추가
    - 새로고침없이 수정사항을 바로 볼수있게 gulp watch 추가
    - src 폴더의 모든 파일을 감시하도록 세팅

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,9 @@
 import gulp from "gulp";
 import del from "del";
 import fileInclude from "gulp-file-include";
+import webserver from "gulp-webserver";
 
-export const includeFile = () => {
+const includeFile = () => {
   return gulp
     .src(["./src/html/**/*"])
     .pipe(
@@ -13,6 +14,23 @@ export const includeFile = () => {
     )
     .pipe(gulp.dest("./dist/html"));
 };
-export const clean = () => del(["dist"]);
 
-export const dev = gulp.series([clean, includeFile]);
+const ws = () =>
+  gulp.src("./dist").pipe(
+    webserver({
+      livereload: true,
+      open: true,
+    })
+  );
+
+const watch = () => {
+  gulp.watch("./src/**/*", includeFile);
+};
+
+const clean = () => del(["dist"]);
+
+const prepare = gulp.series([clean]);
+const includes = gulp.series([includeFile]);
+const postDev = gulp.series([ws, watch]);
+
+export const dev = gulp.series([prepare, includes, postDev]);
