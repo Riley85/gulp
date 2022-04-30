8bb1942041f8a4e8a4a8252ed13643a836e06c73
shs <shs3832@gmail.com>
2022-04-30 19:24:33 +0900

    # 태스크 업데이트 - 클린 프로세스, includeFile 순차실행 - include 기본 경로설정

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,18 @@
 import gulp from "gulp";
 import del from "del";
+import fileInclude from "gulp-file-include";
 
-const routes = {};
-
+export const includeFile = () => {
+  return gulp
+    .src(["./src/html/*"])
+    .pipe(
+      fileInclude({
+        prefix: "@@",
+        basepath: "./src/assets/front/include",
+      })
+    )
+    .pipe(gulp.dest("./dest/html"));
+};
 export const clean = () => del(["build"]);
 
-export const dev = gulp.series([clean]);
+export const dev = gulp.series([clean, includeFile]);
