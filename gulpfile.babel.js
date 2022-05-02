9c9e9fda8d6d8bb6b8f586b10a9fec895884c882
shs <shs3832@gmail.com>
2022-05-02 21:30:54 +0900

    # image optimization - 이미지 최적화 패키지 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import del from "del";
 import fileInclude from "gulp-file-include";
 import webserver from "gulp-webserver";
+import GulpImage from "gulp-image";
 
 const includeFile = () => {
   return gulp
@@ -27,9 +28,12 @@ const watch = () => {
   gulp.watch("./src/**/*", includeFile);
 };
 
+const image = () =>
+  gulp.src("./src/assets/front/images/**/*").pipe(GulpImage()).pipe(gulp.dest("./dist/assets/front/images"));
+
 const clean = () => del(["dist"]);
 
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, image]);
 const includes = gulp.series([includeFile]);
 const postDev = gulp.series([ws, watch]);
 
