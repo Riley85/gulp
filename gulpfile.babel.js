f7557ad6223135ac348ee44cecfb52acfb3f67bc
shs <shs3832@gmail.com>
2022-05-02 21:54:06 +0900

    # gulp sass - dart sass 를 활용한 gulp sass 패키지 작업

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import del from "del";
 import fileInclude from "gulp-file-include";
 import webserver from "gulp-webserver";
 import GulpImage from "gulp-image";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
 
 const includeFile = () => {
   return gulp
@@ -24,17 +27,24 @@ const ws = () =>
     })
   );
 
-const watch = () => {
-  gulp.watch("./src/**/*", includeFile);
-};
-
 const image = () =>
   gulp.src("./src/assets/front/images/**/*").pipe(GulpImage()).pipe(gulp.dest("./dist/assets/front/images"));
+
+const styles = () => {
+  return gulp
+    .src("./src/assets/front/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./dist/assets/front/css"));
+};
+const watch = () => {
+  gulp.watch("./src/**/*", includeFile);
+  gulp.watch("./src/assets/front/scss/**/*.scss", styles);
+};
 
 const clean = () => del(["dist"]);
 
 const prepare = gulp.series([clean, image]);
-const includes = gulp.series([includeFile]);
+const includes = gulp.series([includeFile, styles]);
 const postDev = gulp.series([ws, watch]);
 
 export const dev = gulp.series([prepare, includes, postDev]);
